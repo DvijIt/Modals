4c8c2c37c4caf2d5be3f42a12f2b0accdab09da4
Andrey <dvij@iMac.local>
2020-04-16 20:12:07 +0300

    init repo

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+    <!-- <header></header> -->
+
+    <div class="wrapper-content" @click="stop">
+      <section>
+        <div class="container">
+         
+        </div>
+      </section>
+    </div>
+
+    <!-- <footer></footer> -->
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+};
+</script>
+
+<style>
+</style>
