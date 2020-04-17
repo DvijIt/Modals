7c182a3c72a39bc81acaf534c585729c4808c60f
Andrey <dvij@iMac.local>
2020-04-17 09:44:53 +0300

    ready modals to reuse

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,37 @@
   <div class="wrapper">
     <!-- <header></header> -->
 
-    <div class="wrapper-content" @click="stop">
+    <div class="wrapper-content">
       <section>
         <div class="container">
-         
+          <!-- First Modal -->
+          <button class="btn btnPrimary" @click="modalFirst = true">First Modal</button>
+          <modals v-show="modalFirst" title="First Modal" @close="modalFirst = false">
+            <div slot="body">
+              <p>Text Text</p>
+              <button class="btn btnPrimary" @click="modalFirst = false">Well Done!</button>
+            </div>
+          </modals>
+
+          <!-- Second Modal -->
+          <button class="btn btnPrimary" @click="modalSecond.show = true">Modal With Form</button>
+          <modals
+            v-show="modalSecond.show"
+            title="Modal With Form"
+            @close="modalSecond.show = false"
+          >
+            <form slot="body" @submit.prevent="submitSecondForm">
+              <label>Name:</label>
+              <input type="text" v-model="modalSecond.name" />
+              <label>Email:</label>
+              <input type="email" v-model="modalSecond.email" />
+              <button class="btn btnPrimary">Submit</button>
+            </form>
+          </modals>
+
+          <!-- Vuelidate Modal -->
+          <button class="btn btnPrimary" @click="modalValidate = true">Modal With Form + Validate</button>
+          <modalVuelidate v-show="modalValidate" title="Modal With Form + Validate" @close="close" />
         </div>
       </section>
     </div>
@@ -15,23 +42,46 @@
 </template>
 
 <script>
-
+import modals from "@/components/UI/Modal.vue";
+import modalVuelidate from "@/components/ModalVuelidate.vue";
 
 export default {
   components: {
-
+    modals,
+    modalVuelidate
   },
   data() {
     return {
-
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: "",
+        email: ""
+      },
+      modalValidate: false
     };
   },
   methods: {
-
+    submitSecondForm() {
+      const user = {
+        name: this.modalSecond.name,
+        email: this.modalSecond.email
+      };
+      console.log(user);
+      this.modalSecond.name = "";
+      this.modalSecond.email = "";
+      this.modalSecond.show = false;
+    },
+    close($v) {
+      this.modalValidate = false;
+      $v.name.$model = ''
+      $v.email.$model = ''
+      $v.password.$model = ''
+      $v.repeatPassword.$model = ''
+      $v.$reset()
+    }
   },
-  computed: {
-
-  }
+  computed: {}
 };
 </script>
 
---
+++ src/components/ModalVuelidate.vue
@@ -0,0 +1,121 @@
+<template>
+  <modal title="Modal With Form + Validate" @close="$emit('close', $v)">
+    <form slot="body" @submit.prevent="onSubmit">
+      <!-- name -->
+      <div class="form-item" :class="{ errorInput: $v.name.$error }">
+        <label>Name:</label>
+        <p class="errorText" v-if="!$v.name.required">Field is required!</p>
+        <p
+          class="errorText"
+          v-if="!$v.name.minLength"
+        >Name must have {{$v.name.$params.minLength.min}}!</p>
+        <input v-model="name" :class="{ error: $v.name.$error }" @change="$v.name.$touch()" />
+      </div>
+      <!-- email -->
+      <div class="form-item" :class="{ errorInput: $v.email.$error }">
+        <label>Email:</label>
+        <p class="errorText" v-if="!$v.email.required">Field is required!</p>
+        <p class="errorText" v-if="!$v.email.email">Email is not correct!</p>
+        <input v-model="email" :class="{ error: $v.email.$error }" @change="$v.email.$touch()" />
+      </div>
+
+      <!-- password -->
+      <div class="form-item" :class="{ errorInput: $v.password.$error }">
+        <p class="errorText" v-if="!$v.password.required">Password is required.</p>
+        <p
+          class="errorText"
+          v-if="!$v.password.minLength"
+        >Password must have at least {{ $v.password.$params.minLength.min }} letters.</p>
+        <label>Password</label>
+        <input
+          v-model="password"
+          :class="{ error: $v.password.$error }"
+          @change="$v.password.$touch()"
+        />
+      </div>
+
+      <!-- repeatPassword -->
+      <div class="form-item" :class="{ errorInput: $v.repeatPassword.$error }">
+        <p class="errorText" v-if="!$v.repeatPassword.sameAsPassword">Passwords must be identical.</p>
+        <label>Repeat password</label>
+        <input
+          v-model="repeatPassword"
+          :class="{ error: $v.repeatPassword.$error }"
+          @change="$v.repeatPassword.$touch()"
+        />
+      </div>
+      <button class="btn btnPrimary">Submit</button>
+    </form>
+  </modal>
+</template>
+
+<script>
+import { required, minLength, email, sameAs } from "vuelidate/lib/validators";
+import modal from "@/components/UI/Modal.vue";
+export default {
+  components: {
+    modal
+  },
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: '',
+      repeatPassword: ''
+    };
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4)
+    },
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    },
+    repeatPassword: {
+      sameAsPassword: sameAs("password")
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        const user = {
+          name: this.email,
+          email: this.email,
+          password: this.password
+        };
+        console.log(user);
+        this.name = "";
+        this.email = "";
+        this.password = '';
+        this.repeatPassword = '';
+        this.$v.$reset();
+        this.$emit("close", this.$v);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.form-item .errorText {
+  display: none;
+  margin-bottom: 8px;
+  font-size: 13.4px;
+  color: #de4040;
+}
+.form-item {
+  &.errorInput .errorText {
+    display: block;
+  }
+}
+input.error {
+  border-color: #de4040;
+}
+</style>
